9d2c30fcf87544d51f09db209b6dbf50ba96133a
Calvin Hui <cowfun87@gmail.com>
2017-10-05 14:00:34 -0700

    Setup forms and gather method inside welcome.js

--- routes/welcome.js
+++ routes/welcome.js
@@ -9,4 +9,10 @@ router.get('/about', (request, response) => {
   response.render('about')
 })
 
+router.post('/', (request, response) => {
+  console.log(request.body)
+  const {body} = request;
+  response.render('index', body)
+})
+
 module.exports = router;
